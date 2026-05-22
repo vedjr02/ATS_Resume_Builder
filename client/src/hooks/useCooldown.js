5becae0006a41a3cce52896f994420958f3fc0f7
Vedjr02 <ambreved3@gmail.com>
2026-05-22 11:23:35 +0100

    feat: add cooldown countdown interval

--- client/src/hooks/useCooldown.js
+++ client/src/hooks/useCooldown.js
@@ -4,3 +4,13 @@ import { RATE_LIMIT_COOLDOWN_SECONDS } from '../utils/rateLimit.js';
 export function useCooldown() {
   const [cooldownSec, setCooldownSec] = useState(0);
 
+  useEffect(() => {
+    if (cooldownSec <= 0) return undefined;
+
+    const timer = setInterval(() => {
+      setCooldownSec((current) => (current <= 1 ? 0 : current - 1));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [cooldownSec]);
+
