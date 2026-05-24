4e8ac323a58c1e2d482d4874a456bdd7f375fb93
Vedjr02 <ambreved3@gmail.com>
2026-05-24 19:15:56 +0100

    ui(polish): token DURATION_SLOW

--- client/src/design/tokens.js
+++ client/src/design/tokens.js
@@ -65,3 +65,5 @@ export const TOKEN_EASE_OUT = 'cubic-bezier(0.16,1,0.3,1)';
 export const TOKEN_DURATION_FAST = '150ms';
 
 export const TOKEN_DURATION_BASE = '300ms';
+
+export const TOKEN_DURATION_SLOW = '500ms';
