1cb955df3ebd9d37eb755720a91d228a1093cae6
Vedjr02 <ambreved3@gmail.com>
2026-05-24 19:16:23 +0100

    ui(polish): token INSET_RING

--- client/src/design/tokens.js
+++ client/src/design/tokens.js
@@ -103,3 +103,5 @@ export const TOKEN_SURFACE_800 = 'bg-obsidian-800';
 export const TOKEN_SURFACE_900 = 'bg-obsidian-900';
 
 export const TOKEN_SURFACE_950 = 'bg-obsidian-950';
+
+export const TOKEN_INSET_RING = 'ring-inset';
