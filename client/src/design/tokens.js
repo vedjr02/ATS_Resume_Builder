4d823b79ce905f21bff79464e7b9e6ab2d5f1a57
Vedjr02 <ambreved3@gmail.com>
2026-05-24 19:16:41 +0100

    ui(polish): token HOVER_BRIGHT

--- client/src/design/tokens.js
+++ client/src/design/tokens.js
@@ -129,3 +129,5 @@ export const TOKEN_POINTER_NONE = 'pointer-events-none';
 export const TOKEN_OVERFLOW_HIDDEN = 'overflow-hidden';
 
 export const TOKEN_TRANSITION_ALL = 'transition-all';
+
+export const TOKEN_HOVER_BRIGHT = 'brightness-110';
